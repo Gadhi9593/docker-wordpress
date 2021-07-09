9e5f6ed5aa7c8b5fa39545726cf38724f709e08a
Gadhi Sreenivasa Reddy <76139252+Gadhi9593@users.noreply.github.com>
2021-07-09 20:05:51 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 # For Master Node 
 
 sudo apt-get update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
